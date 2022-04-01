ec860ac3f44d08b867cbfdc5ae5f59b7d5162a7f
tim.staats@bornholdtlee.de <tim.staats@bornholdtlee.de>
2022-04-01 15:55:26 +0200

    basics: - add test

--- app/src/test/java/de/digitaldealer/cardsplease/DeckHelperTest.kt
+++ app/src/test/java/de/digitaldealer/cardsplease/DeckHelperTest.kt
@@ -1,8 +1,11 @@
 package de.digitaldealer.cardsplease
 
 import de.digitaldealer.cardsplease.domain.model.DeckHelper
+import de.digitaldealer.cardsplease.domain.model.Hand
+import de.digitaldealer.cardsplease.extensions.second
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class DeckHelperTest : BaseTest() {
 
@@ -40,5 +43,20 @@ class DeckHelperTest : BaseTest() {
     fun get_complete_shuffled_deck() {
         val cards = DeckHelper.getRandomCardsByAmount(52)
         assertEquals(52, cards.size)
+    }
+
+    @Test
+    fun deal_hand_card_to_two_players() {
+        val cards = DeckHelper.getRandomCardsByPlayerCount(2).toList()
+        val hand1 = Hand(cards.first(), cards.second(), round = 0)
+        val hand2 = Hand(cards[2], cards[3], round = 0)
+
+        assertTrue { hand1.one != hand1.two }
+        assertTrue { hand2.one != hand2.two }
+
+        assertTrue { hand1.one != hand2.one }
+        assertTrue { hand1.one != hand2.two }
+        assertTrue { hand1.two != hand2.two }
+        assertTrue { hand1.two != hand2.one }
     }
 }
